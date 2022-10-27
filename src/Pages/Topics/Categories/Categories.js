6ac2879bd4fca69eef8a6e5d105bcafcd6c02bf8
Iqbaleco <iqbaleco70@gmail.com>
2022-10-27 21:25:04 +0600

    added some data

--- src/Pages/Topics/Categories/Categories.js
+++ src/Pages/Topics/Categories/Categories.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 const Categories = () => {
     const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
     return (
         <div>
             <h3>All categories here</h3>
